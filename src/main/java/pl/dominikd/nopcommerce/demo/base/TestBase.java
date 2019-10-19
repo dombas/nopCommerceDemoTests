d7af6fd41bd1cc112fb31a7e2bc2824e3065ed73
Dominik <dombastik@o2.pl>
2019-10-19 11:58:14 +0200

    Changed to firefox (gecko) driver

--- src/main/java/pl/dominikd/nopcommerce/demo/base/TestBase.java
+++ src/main/java/pl/dominikd/nopcommerce/demo/base/TestBase.java
@@ -2,6 +2,7 @@ package pl.dominikd.nopcommerce.demo.base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
@@ -16,18 +17,22 @@ public class TestBase {
     @BeforeSuite
     public void setChromePath() {
         System.setProperty("webdriver.chrome.driver", "C:\\IdeaProjects\\demotests\\chromedriver.exe");
+        System.setProperty("webdriver.firefox.bin", "C:\\Program Files\\Mozilla Firefox\\firefox.exe");
+        System.setProperty("webdriver.gecko.driver", "G:\\IdeaProjects\\nop\\geckodriver.exe");
     }
 
     @BeforeTest
     public void setUp() {
-        driver = new ChromeDriver();
+        //driver = new ChromeDriver();
+        driver = new FirefoxDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @AfterTest
     public void tearDown() {
-        driver.close();
+        // chrome driver leaves processes running unless we first close() and then quit()
+        //driver.close();
         driver.quit();
     }
 
